94ece934d3a515fcb97724db9b3fbcc1199bc4bf
Ilya <Yegorov>
2023-01-05 12:20:12 +0200

    Carousel added

---
+++ src/components/UI/Carousel/Carousel.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import classes from "./Carousel.module.css";
+
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpeg";
+
+
+const items = [
+    <div className={classes.carousel_container}>
+      <img className={classes.carousel_img} src={img1} alt='img'/>
+    </div>,
+    <div className={classes.carousel_container}>
+        <img className={classes.carousel_img} src={img2} alt='img'/>
+    </div>,
+    <div className={classes.carousel_container}>
+        <img className={classes.carousel_img} src={img3} alt='img'/>
+    </div>
+]
+
+const Carousel = () => (
+  <AliceCarousel
+    autoPlay={true}
+    autoPlayInterval={5000}
+    infinite
+    mouseTracking
+    items={items}
+  />
+);
+
+export default Carousel;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Carousel from "../../components/UI/Carousel/Carousel";
+
+const MainPage = () => {
+    return (
+        <main>
+            <h1>Типографія ПромЕнерго</h1>
+            <Carousel/>
+        </main>
+    );
+};
+
+export default MainPage;
