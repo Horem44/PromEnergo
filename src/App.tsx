999b6087aeba73f2ce4e29f4855dfd6e5722bddd
Ilya <Yegorov>
2023-01-05 12:16:10 +0200

    Carousel added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {Route, Switch} from "react-router-dom";
 import MainHeader from "./components/Navigation/MainHeader";
 import MainFooter from "./components/Navigation/MainFooter";
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
     return (
         <>
             <MainHeader/>
+            <div style={{height: "90px"}}></div>
             <Switch>
-                <Route path="/main" exact>
-
+                <Route path="/" exact>
+                    <MainPage/>
                 </Route>
             </Switch>
-            <div style={{height: '100vh'}}></div>
+            <div style={{height: "100vh"}}></div>
             <MainFooter/>
         </>
     );
